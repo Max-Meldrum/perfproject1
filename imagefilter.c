bfe072a385faa3f5d603587b561d69396e13008c
johan-bjareholt <johanthepro@gmail.com>
2016-11-28 12:50:30 +0100

    Initial work on c port

--- imagefilter.c
+++ imagefilter.c
@@ -1,4 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define W 251
+#define H 256
 
 int main(int argc, char* argv){
+    FILE* fin = fopen("roller1.raw", "r");
+    FILE* fout = fopen("roller2.raw", "w");
+
+    char in[W][H];
+    char out[W][H];
+
+    // Read raw file into array
+    fread(in, W*H, sizeof(char), fin);
+    // Copy in array to out (for the border pixels), can be optimized
+    memcpy(out, in, sizeof(out));
+
+    // Blur
+    for (int x=1; x<W-1; x++){ // Iterate X
+        for (int y=1; y < H-1; y++){ // Iterate Y
+            int sum = 0;
+            for (int xa=-1; xa<=1; xa++){ // Iterate X offset: -1,0,1
+                for (int ya=-1; ya<=1; ya++){ // Iterate Y offset: -1,0,1
+                    sum += in[x+xa][y+ya];
+                }
+            }
+            // bordering pixels have 50% weight and original pixel has 50% weight
+            out[x][y] = ( sum + in[x][y]*7 ) / 16;
+        }
+    }
+
+    fwrite(out, W*H, sizeof(char), fout);
+
     return 0;
 }
