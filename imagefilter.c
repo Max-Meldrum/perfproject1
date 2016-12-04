a2e00bb3bc0a72aaa78aaa214843041bd223e58b
johan-bjareholt <johanthepro@gmail.com>
2016-12-04 12:51:55 +0100

    Added iterations to C implementation

--- imagefilter.c
+++ imagefilter.c
@@ -12,22 +12,29 @@ int main(int argc, char* argv){
     char in[W][H];
     char out[W][H];
 
+    int itercount;
+    printf("iterations: ");
+    scanf("%d", &itercount);
+
     // Read raw file into array
     fread(in, W*H, sizeof(char), fin);
     // Copy in array to out (for the border pixels), can be optimized
     memcpy(out, in, sizeof(out));
 
     // Blur
-    for (int x=1; x<W-1; x++){ // Iterate X
-        for (int y=1; y < H-1; y++){ // Iterate Y
-            int sum = 0;
-            for (int xa=-1; xa<=1; xa++){ // Iterate X offset: -1,0,1
-                for (int ya=-1; ya<=1; ya++){ // Iterate Y offset: -1,0,1
-                    sum += in[x+xa][y+ya];
+    for (int iter=0; iter < itercount; iter++){ // Number of blur iterations
+        for (int x=1; x<W-1; x++){ // Iterate X
+            for (int y=1; y < H-1; y++){ // Iterate Y
+                int sum = 0;
+                for (int xa=-1; xa<=1; xa++){ // Iterate X offset: -1,0,1
+                    for (int ya=-1; ya<=1; ya++){ // Iterate Y offset: -1,0,1
+                        sum += in[x+xa][y+ya];
+                    }
                 }
+                sum -= in[x][y];
+                // bordering pixels have 50% weight and original pixel has 50% weight
+                out[x][y] = (( sum + in[x][y]*7 ) / 16 ) % 255;
             }
-            // bordering pixels have 50% weight and original pixel has 50% weight
-            out[x][y] = ( sum + in[x][y]*7 ) / 16;
         }
     }
 
