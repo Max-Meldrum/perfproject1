29a6b6b4a59ce01192205a48f7c5be90fd0ba026
johan-bjareholt <johanthepro@gmail.com>
2016-12-06 14:46:06 +0100

    Added avg,min,max to dopescript

--- dopescript.sh
+++ dopescript.sh
@@ -3,9 +3,30 @@
 function run(){
     prog=$1
     iters=$2
-    echo -e "$iters \t"$({ time $(echo $iters | $prog >/dev/null); } 2>&1 | grep real | sed -e 's/.*m\([0-9]\+\.[0-9]\+\)s/\1/')
+    min=99999.0
+    max=0
+    sum=0
+    for i in $(seq 1 10); do
+        t=$({ time $(echo $iters | $prog >/dev/null); } 2>&1 | grep real | sed -e 's/.*m\([0-9]\+\.[0-9]\+\)s/\1/')
+        sum=$(bc -l <<< "$sum+$t")
+        if [ $(bc -l <<< "$t<$min") -eq 1 ]; then
+            min=$t
+        fi
+        if [ $(bc -l <<< "$t>$max") -eq 1 ]; then
+            max=$t
+        fi
+    done
+    res=$(bc -l <<< "$sum/10")
+    echo -e "$iters\t$res\t$min\t$max"
+    #echo -e "$iters \t"$({ time $(echo $iters | $prog >/dev/null); } 2>&1 | grep real | sed -e 's/.*m\([0-9]\+\.[0-9]\+\)s/\1/')
 }
 
+# Warmup processor
+echo "Ramping up CPU scheduler for stress test"
+echo 1000 | ./imagefilter_c >/dev/null
+
+# GO!
+echo "Starting benchmark"
 run ./imagefilter_c 1
 run ./imagefilter_c 10
 run ./imagefilter_c 25
